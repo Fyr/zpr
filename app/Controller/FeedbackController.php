f272d9b6e836779a017fbaffdc6878075243916b
Fyr <fyr.work@gmail.com>
2014-06-21 12:10:46 +0200

    Таск по отправке емейлов

--- app/Controller/FeedbackController.php
+++ app/Controller/FeedbackController.php
@@ -34,42 +34,28 @@ class FeedbackController extends AppController {
     public function add() {
         $success = true;
         $data    = array();
+        try {
 
-        if (!isset($this->request->data['email']) or empty($this->request->data['email'])) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'no email specified';
-        } elseif (mb_strlen($this->request->data['email']) > 100) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'email is too long';
-        } elseif (!$this->Feedback->validates(array('fieldList' => array('email')))) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'email is not valid';
-        }
+	        if (!isset($this->request->data['email']) or empty($this->request->data['email'])) {
+	            throw new Exception("no email specified");
+	        } elseif (mb_strlen($this->request->data['email']) > 100) {
+	            throw new Exception('email is too long');
+	        } elseif (!$this->Feedback->validates(array('fieldList' => array('email')))) {
+	            throw new Exception('email is not valid');
+	        }
+	
+	        if (!isset($this->request->data['subject']) or empty($this->request->data['subject'])) {
+	            throw new Exception('no subject specified');
+	        } elseif (mb_strlen($this->request->data['subject']) > 100) {
+	            throw new Exception('subject is too long');
+	        }
+	
+	        if (!isset($this->request->data['text']) or empty($this->request->data['text'])) {
+	        	throw new Exception('no text specified');
+	        } elseif (mb_strlen($this->request->data['text']) > 1000) {
+	        	throw new Exception('text is too long');
+	        }
 
-        if (!isset($this->request->data['subject']) or empty($this->request->data['subject'])) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'no subject specified';
-        } elseif (mb_strlen($this->request->data['subject']) > 100) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'subject is too long';
-        }
-
-        if (!isset($this->request->data['text']) or empty($this->request->data['text'])) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'no text specified';
-        } elseif (mb_strlen($this->request->data['text']) > 1000) {
-            $this->response->statusCode(400);
-            $success = false;
-            $data    = 'text is too long';
-        }
-
-        if ($success) {
             $this->Feedback->create();
 
             $feedback_data = array();
@@ -82,51 +68,44 @@ class FeedbackController extends AppController {
             $feedback_data['text']    = $this->request->data['text'];
             $feedback_data['status']  = Feedback::ID_STATUS_NEW;
 
-            try {
-                if (!$this->Feedback->save(array('Feedback' => $feedback_data))) {
-                    throw new Exception("can't save feedback");
-                }
-
-                $data = $this->Feedback->findById($this->Feedback->id);
-                $this->set('data', $data['Feedback']);
-
-                // уведомление для админа
-                $this->Email->from     = Configure::read('support_email_from');
-                $this->Email->to       = Configure::read('support_email_to');
-                $this->Email->subject  = 'New feedback registered';
-                $this->Email->sendAs   = 'html';
-                $this->Email->template = 'feedback/support_notify';
-
-                if (!$this->Email->send()) {
-                    throw new Exception("can't send email to support");
-                }
-
-                // уведомление для пользователя
-                $this->Email->from     = Configure::read('support_email_from');
-                $this->Email->to       = $feedback_data['email'];
-                $this->Email->subject  = 'Your feedback was registered';
-                $this->Email->sendAs   = 'html';
-                $this->Email->template = 'feedback/user_notify';
-
-                if (!$this->Email->send()) {
-                    throw new Exception("can't send email to user");
-                }
-
-                // выставить статус успешной доставки
-                if (!$this->Feedback->saveField('status', Feedback::ID_STATUS_NOTIFICATION_SENT)) {
-                    throw new Exception("can't change feedback status");
-                }
-            } catch (Exception $e) {
-                $success = false;
+            if (!$this->Feedback->save(array('Feedback' => $feedback_data))) {
+                throw new Exception("can't save feedback");
             }
 
-            if (!$success) {
-                $this->response->statusCode(500);
-                $data = 'failed to save feedback';
-            }
-        }
+            $data = $this->Feedback->findById($this->Feedback->id);
+            $this->set('data', $data['Feedback']);
 
-        $answer = array('status' => ($success ? 'success' : 'error'), 'data' => $data);
-        $this->set(compact('answer'));
+            // уведомление для админа
+            $this->Email->from     = Configure::read('support_email_from');
+            $this->Email->to       = Configure::read('support_email_to');
+            $this->Email->subject  = __('New feedback from user was registered');
+            $this->Email->sendAs   = 'html';
+            $this->Email->template = 'feedback/support_notify';
+
+            if (!$this->Email->send()) {
+                throw new Exception("Can't send email to support");
+            }
+
+            // уведомление для пользователя
+            $this->Email->from     = Configure::read('support_email_from');
+            $this->Email->to       = $feedback_data['email'];
+            $this->Email->subject  = __('Your feedback was registered');
+            $this->Email->sendAs   = 'html';
+            $this->Email->template = 'feedback/user_notify';
+
+            if (!$this->Email->send()) {
+                throw new Exception("Can't send email to user");
+            }
+
+            // выставить статус успешной доставки
+            if (!$this->Feedback->saveField('status', Feedback::ID_STATUS_NOTIFICATION_SENT)) {
+                throw new Exception("can't change feedback status");
+            }
+            
+            $this->setResponse($data);
+        } catch (Exception $e) {
+			$this->setError($e->getMessage());
+		}
+        
     }
 }
